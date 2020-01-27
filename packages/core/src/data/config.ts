5007bbdd2966bdb9064ea65f0c7df3e1a49fd03e
Vjacheslav Trushkin <cyberalien@gmail.com>
2020-01-27 11:07:21 +0200

    Change API stuff in default configuration

--- packages/core/src/data/config.ts
+++ packages/core/src/data/config.ts
@@ -46,30 +46,13 @@ const defaultConfig: DataStorage = {
 	// API
 	API: (({
 		// API hosts
-		resources: [
-			/**
-			 * Redundancy for API servers.
-			 *
-			 * API should have very high uptime because of implemented redundancy at server level, but
-			 * sometimes bad things happen. On internet 100% uptime is not possible.
-			 *
-			 * There could be routing problems. Server might go down for whatever reason, but it takes
-			 * few minutes to detect that downtime, so during those few minutes API might not be accessible.
-			 *
-			 * This script has some redundancy to mitigate possible network issues.
-			 *
-			 * If one host cannot be reached in 'rotate' (750 by default) ms, script will try to retrieve
-			 * data from different host. Hosts have different configurations, pointing to different
-			 * API servers hosted at different providers.
-			 */
-			'https://api.iconify.design',
-		].concat(fallBackAPI),
+		resources: ['https://api.iconify.design'].concat(fallBackAPI),
 
 		// Timeout before next host is used.
 		rotate: 750,
 
-		// Timeout to retry same host. Change it to lower number if there is only 1 available host
-		timeout: 5000,
+		// Timeout to retry same host.
+		timeout: fallBackAPI.length > 0 ? 5000 : 3000,
 
 		// Number of attempts for each host.
 		limit: 2,
