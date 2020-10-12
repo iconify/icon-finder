c60d20595859d02f7a6fac93abeeeaaef034bd36
Vjacheslav Trushkin <cyberalien@gmail.com>
2020-10-12 10:15:12 +0300

    Add iconify--line-md class to code samples for line-md demo

--- ui/line-md/src/components/misc/code-samples.ts
+++ ui/line-md/src/components/misc/code-samples.ts
@@ -29,12 +29,34 @@ export function getIconCode(
 		providerConfig
 	);
 	if (result && lang !== 'svg-uri') {
+		// Add link to footer
 		result.footer = {
 			text:
 				'Do not forget to add stylesheet to your page if you want animated icons:',
 			code:
 				'<link rel="stylesheet" href="https://code.iconify.design/css/line-md.css">',
 		};
+
+		// Modify code
+		if (result.component) {
+			if (typeof result.component.use === 'string') {
+				// Add class
+				result.component.use = result.component.use
+					// Rect and Svelte
+					.replace(
+						'Icon icon={',
+						'Icon class' +
+							(lang === 'svelte' ? '' : 'Name') +
+							'="iconify--line-md" icon={'
+					)
+					// Vue
+					.replace(
+						'Icon :icon',
+						'Icon class="iconify--line-md" :icon'
+					);
+			}
+			console.log(result.component);
+		}
 	}
 
 	return result;
